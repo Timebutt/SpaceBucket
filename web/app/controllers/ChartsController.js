b796697d7f4fa3990894574f5c33d074c251e285
Timebutt <nilstijtgat@gmail.com>
2017-03-21 01:19:30 +0100

    Chart update

--- web/app/controllers/ChartsController.js
+++ web/app/controllers/ChartsController.js
@@ -7,12 +7,12 @@
         ]);
 
     function ChartsController($http, $scope) {
-        var vm = this;
 
 		// Get temperature data
 		$http.get("/SpaceBucket/data/temperature_SpaceBucket_LOG.csv").then(function(response) {
 			data = $.csv.toArrays(response.data);
 			data.splice(0,1);
+            data.splice(0, data.length - 100);
 
 			$scope.labels = data.map(function(value,index) { return value[0]; });
 			$scope.data[0] = data.map(function(value, index) { return value[1]; });
@@ -22,39 +22,53 @@
 		$http.get("/SpaceBucket/data/humidity_SpaceBucket_LOG.csv").then(function(response) {
 			data = $.csv.toArrays(response.data);
 			data.splice(0,1);
+            data.splice(0, data.length - 100);
 
 			$scope.labels = data.map(function(value,index) { return value[0]; });
 			$scope.data[1] = data.map(function(value, index) { return value[1]; });
 		});
 
 		// Get moisture data
-		$http.get("/SpaceBucket/data/moisture_SpaceBucket_LOG.csv").then(function(response) {			
+		$http.get("/SpaceBucket/data/moisture_SpaceBucket_LOG.csv").then(function(response) {
 			data = $.csv.toArrays(response.data);
 			data.splice(0,1);
+            data.splice(0, data.length - 100);
 
 			$scope.labels = data.map(function(value,index) { return value[0]; });
 			$scope.data[2] = data.map(function(value, index) { return value[1]; });
 		});
 
-		$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+		$scope.labels = [];
 		$scope.series = ['Temperature', 'Humidity', 'Moisture'];
 		$scope.data = [
 			[0],
 			[0],
 			[0]
 		];
-		$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+		$scope.datasetOverride = [
+            {
+                yAxisID: 'A'
+            },
+            {
+                yAxisID: 'B'
+            },
+            {
+                yAxisID: 'B'
+            }];
 		$scope.options = {
 			scales: {
 				yAxes: [
 				{
-				  id: 'y-axis-1',
-				  type: 'linear',
-				  display: true,
-				  position: 'left'
+                    id: 'A',
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    ticks: {
+
+                    }
 				},
 				{
-				  id: 'y-axis-2',
+				  id: 'B',
 				  type: 'linear',
 				  display: true,
 				  position: 'right'
