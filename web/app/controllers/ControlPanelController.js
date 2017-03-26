7a8c1aa07eab3d8ea4b2a70a13a18c907016716b
Timebutt <nilstijtgat@gmail.com>
2017-03-26 19:35:58 +0200

    Added power panel behaviour

--- web/app/controllers/ControlPanelController.js
+++ web/app/controllers/ControlPanelController.js
@@ -9,64 +9,64 @@
 
     function ControlPanelController($scope, $rootScope, $interval, $http) {
         var vm = this;
-	var timer = null;
+    	var timer = null;
 
-	// Fetch system status
-	function refreshData() {
+    	// Fetch system status
+    	function refreshData() {
 
-		$http.get("/SpaceBucket/interface.php").then(function(response) { console.log(response.data);
-			$scope.switch_main_light = response.data.main_light;
-			$scope.switch_auxiliary_light = response.data.auxiliary_light;
-			$scope.switch_fan = response.data.fan;
-			$scope.switch_pump = response.data.pump;
-		});
+    		$http.get("/SpaceBucket/interface.php").then(function(response) {
+    			$scope.switch_main_light = response.data.main_light;
+    			$scope.switch_auxiliary_light = response.data.auxiliary_light;
+    			$scope.switch_fan = response.data.fan;
+    			$scope.switch_pump = response.data.pump;
+    		});
 
-	}
+    	}
 
-	// Set periodic update
-	refreshData();
-	timer = $interval(function() {
-		refreshData();
-	}, 10000);
+    	// Set periodic update
+    	refreshData();
+    	timer = $interval(function() {
+    		refreshData();
+    	}, 10000);
 
-	// Save data to server when switch is clicked
-	$scope.$watchGroup(['switch_main_light', 'switch_auxiliary_light', 'switch_fan', 'switch_pump'], function(newValues, oldValues, scope) {
+    	// Save data to server when switch is clicked
+    	$scope.$watchGroup(['switch_main_light', 'switch_auxiliary_light', 'switch_fan', 'switch_pump'], function(newValues, oldValues, scope) {
 
-		// Pause the refreshData() routine
-		$interval.cancel(timer);
+    		// Pause the refreshData() routine
+    		$interval.cancel(timer);
 
-		// Prepare object to be posted
-		var post = {
-			"main_light": {
-				"description": "Main Light",
-				"pin": $rootScope.CONFIG['GPIO'].main_light,
-				"value": $scope.switch_main_light
-			},
-			"auxiliary_light": {
-				"description": "Auxiliary Light",
-				"pin": $rootScope.CONFIG['GPIO'].auxiliary_light,
-				"value": $scope.switch_auxiliary_light
-			},
-			"fan": {
-				"description": "Fan",
-				"pin": $rootScope.CONFIG['GPIO'].fan,
-				"value": $scope.switch_fan
-			},
-			"pump": {
-				"description": "Pump",
-				"pin": $rootScope.CONFIG['GPIO'].pump,
-				"value": $scope.switch_pump
-			}
-		}
+    		// Prepare object to be posted
+    		var post = {
+    			"main_light": {
+    				"description": "Main Light",
+    				"pin": $rootScope.CONFIG['GPIO'].main_light,
+    				"value": $scope.switch_main_light
+    			},
+    			"auxiliary_light": {
+    				"description": "Auxiliary Light",
+    				"pin": $rootScope.CONFIG['GPIO'].auxiliary_light,
+    				"value": $scope.switch_auxiliary_light
+    			},
+    			"fan": {
+    				"description": "Fan",
+    				"pin": $rootScope.CONFIG['GPIO'].fan,
+    				"value": $scope.switch_fan
+    			},
+    			"pump": {
+    				"description": "Pump",
+    				"pin": $rootScope.CONFIG['GPIO'].pump,
+    				"value": $scope.switch_pump
+    			}
+    		}
 
-		// Send the POST request
-		$http.post("/SpaceBucket/interface.php", post)
-		.then(
-			function(response) {
+    		// Send the POST request
+    		$http.post("/SpaceBucket/interface.php", post)
+    		.then(
+    			function(response) {
 
-			},
-			function(error) {
-				console.error("Couldn't POST values: " + error);
+    			},
+    			function(error) {
+    				console.error("Couldn't POST values: " + error);
 			});
 
 		});
--- web/app/controllers/PowerController.js
+++ web/app/controllers/PowerController.js
@@ -2,12 +2,39 @@
     angular
         .module('app')
         .controller('PowerController', [
-			'$http', '$scope',
+			'$http', '$scope', '$rootScope', '$interval',
             PowerController
-        ]);	
-		
-    function PowerController($http, $scope) {
-        var vm = this;       
+        ]);
+
+    function PowerController($http, $scope, $rootScope, $interval) {
+        var vm = this;
+
+        // Fetch system status
+    	function refreshData() {
+
+    		$http.get("/SpaceBucket/data/power.json").then(function(response) {
+
+                // Set run time per consumer (in hours)
+                $scope.hoursMainLight = (response.data['main_light'] / 60).toFixed(2)
+                $scope.hoursAuxiliaryLight = (response.data['auxiliary_light'] / 60).toFixed(2)
+                $scope.hoursPump = (response.data['pump'] / 60).toFixed(2)
+                $scope.hoursFan = (response.data['fan'] / 60).toFixed(2)
+
+                // Calculate power consumption (in kWh)
+                $scope.powerMainLight = (response.data['main_light'] / 60 * $rootScope.CONFIG['powerConsumption'].main_light / 1000).toFixed(2);
+                $scope.powerAuxiliaryLight = (response.data['auxiliary_light'] / 60 * $rootScope.CONFIG['powerConsumption'].auxiliary_light / 1000).toFixed();
+                $scope.powerPump = (response.data['pump'] / 60 * $rootScope.CONFIG['powerConsumption'].pump / 1000).toFixed(2);
+                $scope.powerFan = (response.data['fan'] / 60 * $rootScope.CONFIG['powerConsumption'].fan / 1000).toFixed(2);
+
+    		});
+
+    	}
+
+    	// Set periodic update
+    	refreshData();
+    	timer = $interval(function() {
+    		refreshData();
+    	}, 10000);
 
     }
 })();
